7d02a870d5e837ce439e515ff5d907b48e6c2880
lucas_ruiz_steimetz <steimetz.lucas@gmail.com>
2026-03-26 17:33:51 -0300

    Added CRUD operations

---
+++ backend/src/models/post.model.js
@@ -0,0 +1,33 @@
+import mongoose, {Schema} from "mongoose";
+import { timeStamp } from "node:console";
+
+const postSchema = new Schema(
+    {
+        name:{
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        description:{
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        age:{
+            type: Number,
+            required: true,
+            min: 1,
+            max: 130
+        }
+    },
+    {
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Post = mongoose.model('Post', postSchema)
